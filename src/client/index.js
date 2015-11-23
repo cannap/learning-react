448515fe52ee65607067547c590ba1c1ac7f43b1
cannap <sh4fty@gmail.com>
2015-11-23 01:47:11 +0100

    rename router to root for matching the #root id

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Root from './router';
+import Root from './root';
 import { createHistory, createHashHistory } from 'history';
 
 render(<Root />, document.getElementById('root'));
